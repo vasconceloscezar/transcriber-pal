5cca3ca426ff66571756cea3b9b9bddd48a59602
Cezar Vasconcelos <97035956+vasconceloscezar@users.noreply.github.com>
2023-06-07 21:15:07 -0300

    fix: check multiple audio and video files

--- main.py
+++ main.py
@@ -3,25 +3,34 @@ import asyncio
 from tools.audio_to_text import transcribe_audio
 from tools.video_to_audio import convert_video_to_audio
 
+def check_file_is_audio_or_video(input_file):
+    video_extensions = [".mp4", ".mov", ".avi", ".mkv", ".flv", ".wmv", ".webm"]
+    audio_extensions = [".mp3", ".wav", ".ogg", ".m4a", ".flac" , ".aac" , ".opus"]
+    if input_file.endswith(tuple(audio_extensions)):
+        return "audio"
+    elif input_file.endswith(tuple(video_extensions)):
+        return "video"
+    else:
+        return None
+    
 async def main(input_file):
     if not input_file:
         print("No input file provided.")
         return
-
-    if input_file.endswith(".mp3") or input_file.endswith(".wav"):
+    file_type = check_file_is_audio_or_video(input_file)
+    if file_type == "audio":
         await transcribe_audio(input_file)
-    elif input_file.endswith(".mp4"):
-        audio_path = f"{input_file[:-4]}.mp3"
-        await convert_video_to_audio(input_file, audio_path)
+    elif file_type == "video":
+        audio_path = input_file[:-4] + ".mp3"  # Define the output audio path
+        await convert_video_to_audio(input_file, audio_path)  # Provide audio_path as argument
         await transcribe_audio(audio_path)
-    else:
-        print("Invalid file format. Supported formats are .mp3, .wav, and .mp4.")
+    else: 
+        print("Invalid file format. Supported formats are: mp3, wav, ogg, m4a, flac, aac, opus, mp4, mov, avi, mkv, flv, wmv, webm")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", nargs="?", default="", help="Path to input file")
     args = parser.parse_args()
-
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(args.input_file))
