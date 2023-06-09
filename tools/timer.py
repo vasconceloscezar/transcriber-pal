46d7df4ab418c94cfc2310fa6ebeddd7d7ff16a8
Cezar Vasconcelos <97035956+vasconceloscezar@users.noreply.github.com>
2023-06-09 09:38:33 -0300

    feat: add Timer

---
+++ tools/timer.py
@@ -0,0 +1,20 @@
+import time
+
+
+class Timer:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+        return self.format_time(self.end_time - self.start_time)
+
+    def format_time(self, seconds):
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        seconds = int(seconds % 60)
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
